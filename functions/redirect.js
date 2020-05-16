02ba3de156d6171a99e84b97c21f32815af8e20f
Mike <mikefelix@gmail.com>
2020-05-16 06:47:17 -0400

    Add form submission redirect

---
+++ functions/redirect.js
@@ -0,0 +1,13 @@
+exports.handler = async (event, context) => {
+
+  const redirectUrl = 'https://naughty-lamarr-f5f136.netlify.app/'
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: redirectUrl,
+      'Cache-Control': 'no-cache',
+    },
+    body: JSON.stringify({})
+  }
+}
