ea4ee46332101af53607fe8cb866a63466c1cfbe
Mike <mikefelix@gmail.com>
2020-05-18 06:50:17 -0400

    Refactor header css

--- src/components/header.js
+++ src/components/header.js
@@ -1,63 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 import Icon from "../components/icon"
-import Colors from "../constants/Colors"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: Colors.primary,
-      display: `flex`,
-      justifyContent: `center`
-    }}
-  >
-    <div
-      style={{
-        width: `100%`,
-        maxWidth: `960px`,
-        padding: `.1rem 1.45rem .1rem .5rem`,
-        display: `flex`,
-        flexDirection: `row`,
-        justifyContent: `space-between`,
-        alignItems: `center`,
-      }}
-    >
-      <div
-        style={{
-          display: `flex`,
-          flexDirection: `row`,
-          alignItems: `center`,
-        }}
-      >
-        <Icon/>
-        <h2 style={{ margin: 0, padding: `0 0 .2rem .3rem` }}>
-          <Link
-            to="/"
-            style={{
-              color: `white`,
-              fontFamily: `Roboto`,
-              textDecoration: `none`,
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h2>
-      </div>
-
-      <h6 style={{
-        margin: 0,
-      }}
-      ><Link
-        to="/contact/"
-        style={{
-          color: Colors.secondary,
-          fontFamily: `Roboto`,
-          textDecoration: `none`,
-        }}
-        >
-          Contact
+  <header className="nav">
+    <div className="nav-container">
+      <div className="nav-left">
+        <Link className="link" to="/">
+          <Icon/>
+          <h2 className="site-title">{siteTitle}</h2>
         </Link>
-      </h6>
+      </div>
+      <Link className="link" to="/contact/">
+         <h6 className="nav-item">Contact</h6>
+      </Link>
 
     </div>
   </header>
