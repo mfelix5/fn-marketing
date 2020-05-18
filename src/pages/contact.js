72a10252b678abadbc2b2c016294ac3f22e2fae1
Mike <mikefelix@gmail.com>
2020-05-18 16:06:04 -0400

    Add background images to pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,69 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "./contact.css"
 
-const ContactPage = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <div className="page-container">
-      <p>If you have any issues or feedback on how to improve Farewise, please let us know. We will get back to you shortly. Thanks!</p>
+const ContactPage = () => {
+  const { background } = useStaticQuery(graphql`
+    {
+      background: file(relativePath: { eq: "anders-jilden.jpg" }) {
+        childImageSharp {
+          fluid(quality: 100, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <BackgroundImage className="background-image" fluid={background.childImageSharp.fluid}>
+      <Layout>
+        <SEO title="Contact" />
+        <div className="page-container">
+          <div className="card">
+            <h3>
+              If you have any feedback or issues using Farewise, please let us know and we'll get back to you shortly!
+            </h3>
 
-      <form
-        name="contact"
-        method="POST"
-        // action="https://naughty-lamarr-f5f136.netlify.app/.netlify/functions/redirect"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-      <input type="hidden" name="form-name" value="contact" />
-        <div className="field-container">
-            <input className="form-field" type="text" name="name" id="name" placeholder="Name" />
-            <input className="form-field" type="email" name="email" id="email" placeholder="Email" />
-            <textarea name="message" id="message" placeholder="Message" />
-          <div data-netlify-recaptcha="true"></div>
-          <input
-            className="form-field button"
-            type="submit"
-            value="Send Message"
-           />
+            <form
+              name="contact"
+              method="POST"
+              // action="https://naughty-lamarr-f5f136.netlify.app/.netlify/functions/redirect"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="field-container">
+                <input
+                  className="form-field"
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Name"
+                />
+                <input
+                  className="form-field"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                />
+                <textarea name="message" id="message" placeholder="Message" />
+                <div data-netlify-recaptcha="true"></div>
+                <input
+                  className="form-field button"
+                  type="submit"
+                  value="Send Message"
+                />
+              </div>
+            </form>
+          </div>
         </div>
-      </form>
-
-    </div>
-  </Layout>
-)
+      </Layout>
+    </BackgroundImage>
+  )
+}
 
 export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,44 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Badges from "../components/badges"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Farewise" />
-    <div style={{
-      width: `100%`,
-      maxWidth: `960px`,
-      backgroundColor: `white`,
-      padding: `1.45rem 1.0875rem 1.45rem`,
-      margin: 0
-    }}>
-      <h2>Save on your train commute!</h2>
-      <h4>Farewise recommends the least expensive ticketing options for travel on Long Island Railroad, Metro-North and New Jersey Transit.</h4>
-      <Image />
-      <Badges />
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  const { background } = useStaticQuery(graphql`
+    {
+      background: file(relativePath: { eq: "rafael-leao.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <BackgroundImage
+      className="background-image-index"
+      fluid={background.childImageSharp.fluid}
+    >
+    <Layout>
+        <SEO title="Farewise" />
+        <div style={{
+          width: `100%`,
+          maxWidth: `960px`,
+          padding: `1.45rem 1.0875rem 1.45rem`,
+          margin: 0
+        }}>
+          <h2>Save on your train commute!</h2>
+          <h4>Farewise recommends the least expensive ticketing options for travel on Long Island Railroad, Metro-North and New Jersey Transit.</h4>
+          <Image />
+          <Badges />
+        </div>
+      </Layout>
+    </BackgroundImage>
+  )
+}
 
 export default IndexPage
